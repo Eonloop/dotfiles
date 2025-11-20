61f4f9d105a26f8cf0bff0c56db624b1a0903976
Ian Jones <ianj1993@gmail.com>
2025-11-20 09:45:01 -0700

    Added statement to add .cargo/bin as PATH variable

--- bash-scripts/rust-setup.sh
+++ bash-scripts/rust-setup.sh
@@ -4,3 +4,8 @@
 # This will install the latest stable version of Rust and Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+
+# Rust / Cargo Path
+if [ -d "$HOME/.cargo/bin" ]; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+fi
