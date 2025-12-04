be60101a4654780ccf616fec16e9afbb83b0f0cb
Ian Jones <ianj1993@gmail.com>
2025-12-04 11:06:08 -0700

    Added config files for ghostty, zshrc, and alacritty

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -34,14 +34,4 @@ sudo apt install -y \
 
 
 
-# Optional : Install Rust + Cargo
-# This will install the latest stable version of Rust and Cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Optional : Install command line tools using Cargo
-sudo cargo install -y \
-	bat \
-	eza \
-	rg
-
 echo "✅ Installation complete!"
--- zsh/.zsh/.zprofile
+++
@@ -1,5 +0,0 @@
-
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# Added by `rbenv init` on Thu Aug  1 21:21:28 MDT 2024
-eval "$(rbenv init - --no-rehash zsh)"
